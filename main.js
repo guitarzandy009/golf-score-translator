14ba2a4a74a739932fb2f199a9ec18ca377c0631
Andy Stuckey <andystuckey@mail.com>
2026-05-27 09:52:53 -0400

    write skeleton for the function

--- main.js
+++ main.js
@@ -9,15 +9,36 @@ const names = [
 ];
 
 function golfScore(par, strokes) {
+  let num = par - 2;
   if (strokes === "1") {
     return names[0];
-  } else if (strokes <= par - 2) {
+  } else if () {
     return names[1];
-  } else if (strokes === par - 1) {
+  } else if () {
     return names[2];
-  } else if (strokes === par) {
+  } else if () {
     return names[3];
+  } else if () {
+    return names[4];
+  } else if () {
+    return names[5]
+  } else {
+    return names[6];
   }
 }
 
 console.log("Golf Score Translator");
+console.log(golfScore(1, 1))
+console.log(golfScore(3, 1))
+console.log(golfScore(4, 1))
+console.log(golfScore(5, 1))
+console.log(golfScore(3, 4))
+console.log(golfScore(4, 5))
+console.log(golfScore(5, 6))
+console.log(golfScore(3, 5))
+console.log(golfScore(4, 6))
+console.log(golfScore(5, 7))
+console.log(golfScore(3, 7))
+console.log(golfScore(4, 8))
+console.log(golfScore(4, 9))
+console.log(golfScore(5, 9))
