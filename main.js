8915f8d71a4341c1af1b29e7434cd8569e7149de
Andy Stuckey <andystuckey@mail.com>
2026-05-31 03:37:20 -0400

    finished script via help from codeManS practice videos youtube channel

--- main.js
+++ main.js
@@ -9,36 +9,36 @@ const names = [
 ];
 
 function golfScore(par, strokes) {
-  let num = par - 2;
-  if (strokes === "1") {
+  let num = strokes - par;
+  if (strokes === 1) {
     return names[0];
-  } else if () {
+  } else if (num < -1) {
     return names[1];
-  } else if () {
+  } else if (num < 0) {
     return names[2];
-  } else if () {
+  } else if (num < 1) {
     return names[3];
-  } else if () {
+  } else if (num < 2) {
     return names[4];
-  } else if () {
-    return names[5]
+  } else if (num < 3) {
+    return names[5];
   } else {
     return names[6];
   }
 }
 
 console.log("Golf Score Translator");
-console.log(golfScore(1, 1))
-console.log(golfScore(3, 1))
-console.log(golfScore(4, 1))
-console.log(golfScore(5, 1))
-console.log(golfScore(3, 4))
-console.log(golfScore(4, 5))
-console.log(golfScore(5, 6))
-console.log(golfScore(3, 5))
-console.log(golfScore(4, 6))
-console.log(golfScore(5, 7))
-console.log(golfScore(3, 7))
-console.log(golfScore(4, 8))
-console.log(golfScore(4, 9))
-console.log(golfScore(5, 9))
+console.log(golfScore(1, 1));
+console.log(golfScore(3, 1));
+console.log(golfScore(4, 1));
+console.log(golfScore(5, 1));
+console.log(golfScore(3, 4));
+console.log(golfScore(4, 5));
+console.log(golfScore(5, 6));
+console.log(golfScore(3, 5));
+console.log(golfScore(4, 6));
+console.log(golfScore(5, 7));
+console.log(golfScore(3, 7));
+console.log(golfScore(4, 8));
+console.log(golfScore(4, 9));
+console.log(golfScore(5, 9));
